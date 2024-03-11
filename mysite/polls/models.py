9921a9a5c39c4f853a3a410d3637321892728dc7
Ken Wu <kenwu1009us@gmail.com>
2024-03-11 14:28:16 -0700

    fix the bug

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -9,7 +9,8 @@ class Question(models.Model):
     publish_date = models.DateTimeField("date published")
 
     def was_published_recently(self):
-        return self.publish_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.publish_date <= now
 
     def __str__(self):
         return self.question_text
