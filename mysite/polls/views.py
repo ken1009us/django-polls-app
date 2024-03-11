2b6a32191f6e973c22139c7f5b26ac467d43b513
Ken Wu <kenwu1009us@gmail.com>
2024-03-10 22:29:33 -0700

    add more views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -6,3 +6,15 @@ def index(request):
     return HttpResponse("Hello World!")
 
 
+def detail(request, question_id):
+    response = f"You are looking at the question {question_id}."
+    return HttpResponse(response)
+
+
+def results(request, question_id):
+    response = f"You are looking at the results of question {question_id}."
+    return HttpResponse(response)
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}.")
